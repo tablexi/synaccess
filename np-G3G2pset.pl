52e671544523b0ff3d3ff4195658dd6beb90e7e3
Jason Hanggi <jason@tablexi.com>
2013-03-18 15:46:08 -0500

    add authentication to perl script

--- np-G3G2pset.pl
+++ np-G3G2pset.pl
@@ -12,7 +12,7 @@
 use IO::Socket;
 #filling Synaccess system IP address and  port number below
 
-$sock = new IO::Socket::INET (PeerAddr => '192.168.10.20',
+$sock = new IO::Socket::INET (PeerAddr => '192.168.10.21',
                               PeerPort => 23,
                               Proto    => 'tcp');
 
@@ -27,8 +27,8 @@ $sock->recv($myCap, 1024);
 
 #---------important-----------
 #Must send as startup
-$msgSend = "\r";
-$sock->send($msgSend);
+# $msgSend = "\r";
+# $sock->send($msgSend);
 #---------important-------------
 
 
@@ -38,45 +38,88 @@ $sock->send($msgSend);
 #---------important-------------
 
 
+#---------Login/ID/Password-------------
+# sleep(1);
+#send login command
+# $tMsg = "now sending login cmd\r\n";
+# print $tMsg;
 
+# $msgSend = "login\r";
+# $sock->send($msgSend);
+# sleep(1);
+# $sock->recv($myCap, 1024);
+# $tMsg = "cap'd msg after login cmd\r\n";
+# print $myCap;
+
+
+#send user ID
+# $tMsg = "now sending user ID-outlet4\r\n";
+# print $tMsg;
+$msgSend = "admin\r";
+$sock->send($msgSend);
+#Optinal - to capture and see what's going on
+# $sock->recv($myCap, 1024);
+# print $myCap;
+
+sleep 1;  #may require delay. Min. 100ms.  Here the delay is 1 sec.
+
+#Local Msg
+#print "Send Password";
+# $tMsg = "now sending user PWD\r\n";
+print $tMsg;
+$msgSend = "admin\r";
+$sock->send($msgSend);
+#required delay. Min. 100ms.  Here the delay is 1 sec.
+sleep 1;  #may require delay. Min. 100ms.  Here the delay is 1 sec.
+
+#Optinal - to capture and see what's going on
+$sock->recv($myCap, 1024);
+print $myCap;
+
+sleep 1;
 
 #now sending reboot command "pset 1 0"
 print "Sending command \"pset 1 0\" with  '\\n\\r' only.====>working.\n\r";
 $msg = "pset 1 0\n\r";
 $sock->send($msg);
 sleep 1;
+$sock->recv($myCap, 1024);
+print $myCap;
 $msg = "pset 1 1\n\r";
+print "Sending: $msg";
 $sock->send($msg);
 sleep 1;
 
 #$sock->recv($menu, 2048);
 #print $menu;
 
-#now sending reboot command "pset 2 0"
-print "Sending command \"pset 2 0\" with  '\\r' only.====>working.\n\r";
-$msg = "pset 2 0\r";
-$sock->send($msg);
-sleep 1;
-$msg = "pset 2 1\r";
-$sock->send($msg);
-sleep 1;
+# #now sending reboot command "pset 2 0"
+# print "Sending command \"pset 2 0\" with  '\\r' only.====>working.\n\r";
+# $msg = "pset 2 0\r";
+# $sock->send($msg);
+# sleep 1;
+# $msg = "pset 2 1\r";
+# $sock->send($msg);
+# sleep 1;
 
-#now sending reboot command "pset 1 0"
-#print "Sending command \"pset 1 0\" with  '\\r\\n' only.====>working.\n\r";
-#$msg = "pset 3 0\r\n";
-#$sock->send($msg);
-#sleep 1;
-#$msg = "pset 3 1\r\n";
-#$sock->send($msg);
+# #now sending reboot command "pset 1 0"
+# #print "Sending command \"pset 1 0\" with  '\\r\\n' only.====>working.\n\r";
+# #$msg = "pset 3 0\r\n";
+# #$sock->send($msg);
+# #sleep 1;
+# #$msg = "pset 3 1\r\n";
+# #$sock->send($msg);
 
-#Local Msg
-print "\nLoging out the system\n\r";
-$msg = "logout\r";
-$sock->send($msg);
+# #Local Msg
+# print "\nLoging out the system\n\r";
+# $msg = "logout\r";
+# $sock->send($msg);
 
 #---------Helpful delay -----------
 sleep 1;
 close($sock);
+
+
 
 
 
