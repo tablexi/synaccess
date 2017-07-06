adc4f665df29a1e39e40bae17a222c19e0fc5421
Jason Hanggi <jason@tablexi.com>
2017-07-06 14:51:54 -0500

    Use nested modules for cleaner requires

--- lib/synaccess_connect/net_booter/http/rev_a.rb
+++ lib/synaccess_connect/net_booter/http/rev_a.rb
@@ -1,24 +1,32 @@
-class NetBooter::Http::RevA < NetBooter::HttpConnection
-  # So here's the deal... When you hit the switch page (pwrSw1.cgi) nothing happens.
-  # It's only when hitting the status page AFTER visiting a swith page that
-  # the relay is toggled.
-  def toggle_relay(outlet)
-    get_request("/pwrSw#{outlet}.cgi")
-    get_request("/synOpStatus.shtml")
-  end
+module NetBooter
 
-  def statuses
-    resp   = get_request('/synOpStatus.shtml')
+  module Http
 
-    doc    = Nokogiri::HTML(resp.body)
-    nodes  = doc.xpath('//img')
-    status = {}
+    class RevA < HttpConnection
+      # So here's the deal... When you hit the switch page (pwrSw1.cgi) nothing happens.
+      # It's only when hitting the status page AFTER visiting a swith page that
+      # the relay is toggled.
+      def toggle_relay(outlet)
+        get_request("/pwrSw#{outlet}.cgi")
+        get_request("/synOpStatus.shtml")
+      end
 
-    nodes.each do |node|
-      if node.values[0].match(/^led(off|on).gif$/)
-        status[node.parent.parent.xpath('th').first.content.to_i] = $1 == 'on' ? true : false
+      def statuses
+        resp   = get_request('/synOpStatus.shtml')
+
+        doc    = Nokogiri::HTML(resp.body)
+        nodes  = doc.xpath('//img')
+        status = {}
+
+        nodes.each do |node|
+          if node.values[0].match(/^led(off|on).gif$/)
+            status[node.parent.parent.xpath('th').first.content.to_i] = $1 == 'on' ? true : false
+          end
+        end
+        status
       end
     end
-    status
+
   end
+
 end
--- lib/synaccess_connect/net_booter/telnet/rev_a.rb
+++ lib/synaccess_connect/net_booter/telnet/rev_a.rb
@@ -1,23 +1,33 @@
-class NetBooter::Telnet::RevA < NetBooter::TelnetConnection
+module NetBooter
 
-  def override_options
-    {
-      :prompt => /^\r>$/n,
-      :status_string => 'On'
-    }
+  module Telnet
+
+    class RevA < TelnetConnection
+
+      def override_options
+        {
+          :prompt => /^\r>$/n,
+          :status_string => 'On'
+        }
+      end
+
+      def local_statuses
+        parse_status @connection.cmd("pshow")
+      end
+
+      private
+
+      def authenticate
+        sleep 0.1
+        @connection.puts(@options[:username])
+        sleep 0.1
+        @connection.puts(@options[:password])
+        @connection.cmd('')
+        sleep 0.1
+      end
+
+    end
+
   end
 
-  def local_statuses
-    parse_status @connection.cmd("pshow")
-  end
-
-private
-  def authenticate
-    sleep 0.1
-    @connection.puts(@options[:username])
-    sleep 0.1
-    @connection.puts(@options[:password])
-    @connection.cmd('')
-    sleep 0.1
-  end
 end
